110887cec62f3cbf02dc22595feef45814f72eca
göksel gürlük <gurlukgoksel@gmail.com>
2024-02-19 11:46:38 +0300

    Readme dosyası eklendi kod blokları düzenlendi acıklama sairlari eklendi

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,18 @@ public class Main {
     public static void main(String[] args) {
        
        int a,b,c,islem;
-       
+      //kullanicidan girdi alabilmek icin scanner sınıfını tanımladık
        Scanner input = new Scanner(System.in);
-
-        System.out.print("a sayisini giriniz");
-        a= input.nextInt();
-        System.out.print("b sayisini giriniz");
-        b= input.nextInt();
-        System.out.print("c sayisini giriniz");
-        c= input.nextInt();
-        islem = ( a+b*c-b);
-        System.out.print("SONUC: "+islem);
+        //kullanicidan 3 adet sayı almak ici ekrana bilgi bastırdık ver degiskene atadik
+        System.out.print("a sayisini giriniz: ");
+            a= input.nextInt();
+        System.out.print("b sayisini giriniz: ");
+            b= input.nextInt();
+        System.out.print("c sayisini giriniz: ");
+            c= input.nextInt();
+            //islemi belirledik ve islemi degiskenine atadik
+            islem = (a+b*c-b);
+                //sonucu kullanici göremesi icin ekrana bastirdik
+                System.out.print("SONUC: "+islem);
     }
 }
